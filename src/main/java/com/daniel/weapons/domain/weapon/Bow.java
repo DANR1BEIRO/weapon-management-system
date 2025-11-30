cf0cfea6daab9b07c74dfb29e84a58eba69da2bc
DANR1BEIRO <danribeirodev@gmail.com>
2025-11-30 12:12:02 -0300

    feat(domain): implementa Bow com suporte a municao limitada (Aljava)
    - Adiciona campo AmmoContainer para simular a aljava de flechas.
    - Implementa interface Shootable, mas nao Reloadable (conforme requisito).
    - Reutiliza a logica de contagem da Magazine via interface AmmoContainer.

--- src/main/java/com/daniel/weapons/domain/weapon/Bow.java
+++ src/main/java/com/daniel/weapons/domain/weapon/Bow.java
@@ -2,26 +2,34 @@ package com.daniel.weapons.domain.weapon;
 
 import com.daniel.weapons.domain.ammo.Ammo;
 import com.daniel.weapons.domain.ammo.WeaponType;
+import com.daniel.weapons.domain.magazine.AmmoContainer;
 
 public class Bow extends Weapon implements Shootable {
 
-    public Bow(WeaponType weaponType, String name, Ammo ammo) {
+    private AmmoContainer quiver;
+
+    public Bow(WeaponType weaponType, String name, AmmoContainer quiver) {
         super(weaponType, name);
+        this.quiver = quiver;
     }
 
     @Override
     public void shoot() {
-
-        System.out.println("throwing a arrow");
+        if (quiver.getAmmunition() > 0) {
+            System.out.println(getName() + " dispara uma flecha!");
+            quiver.decreaseAmmo();
+        } else {
+            System.out.println("Sem flechas na aljava!");
+        }
     }
 
     @Override
     public void storeWeapon() {
         if (storedWeapon) {
-            System.out.println(getName() + " já está guardada.");
-            return;
+            System.out.println(getName() + " já está guardado.");
+        } else {
+            System.out.println(getName() + " guardando nas costas junto com a aljava.");
+            storedWeapon = true;
         }
-        System.out.println(getClass().getSimpleName() + " " + getName() + " pendurado no ombro.");
-        storedWeapon = true;
     }
 }

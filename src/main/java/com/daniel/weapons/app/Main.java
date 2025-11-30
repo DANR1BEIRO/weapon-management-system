a347d09e0d9d4f74deced23ddb9eac1a5cb34554
DANR1BEIRO <danribeirodev@gmail.com>
2025-11-30 13:20:57 -0300

    refactor(domain): remove generics da classe Person
    - Substitui Person<T> por Person simples, utilizando polimorfismo (List<Weapon>) para gerenciar o inventário.
    - Remove restrição que impedia ou dificultava a criação de inventários mistos (ex: Rifle + Pistola).

--- src/main/java/com/daniel/weapons/app/Main.java
+++ src/main/java/com/daniel/weapons/app/Main.java
@@ -2,28 +2,26 @@ package com.daniel.weapons.app;
 
 import com.daniel.weapons.domain.ammo.Ammo;
 import com.daniel.weapons.domain.ammo.WeaponType;
+import com.daniel.weapons.domain.magazine.AmmoContainer;
 import com.daniel.weapons.domain.magazine.Magazine;
 import com.daniel.weapons.domain.person.Person;
 import com.daniel.weapons.domain.weapon.*;
+import com.daniel.weapons.service.WeaponService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Weapon> larasWeapons = new ArrayList<>(
-                List.of(new Rifle(WeaponType.RIFLE, "762", new Magazine(Ammo.RIFLE_AMMO)),
-                        new Pistol(WeaponType.PISTOL, "desert eagle", new Magazine(Ammo.PISTOL_AMMO))));
 
-        Person<Weapon> lara = new Person<>("Lara croft", larasWeapons);
-        System.out.println(larasWeapons);
-        for (Weapon larasWeapon : larasWeapons) {
-            larasWeapon.storeWeapon();
-            if (larasWeapon instanceof Reloadable s) {
-                s.reload(20);
+        Person lara = new Person("Lara croft",
+                new ArrayList<>(List.of(
+                        new Rifle(WeaponType.RIFLE, "762", new Magazine(Ammo.RIFLE_AMMO)),
+                        new Pistol(WeaponType.PISTOL, "desert eagle", new Magazine(Ammo.PISTOL_AMMO)),
+                        new Bow(WeaponType.BOW, "Arco da Lara", new Magazine(Ammo.ARROW)))));
 
-
-            }
+        for (Weapon weapon : lara.getWeapons()) {
+            WeaponService.performShoot(weapon, 10);
         }
     }
 }
--- src/main/java/com/daniel/weapons/domain/person/Person.java
+++ src/main/java/com/daniel/weapons/domain/person/Person.java
@@ -2,17 +2,19 @@ package com.daniel.weapons.domain.person;
 
 import com.daniel.weapons.domain.weapon.Weapon;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Person<T extends Weapon> {
+public class Person {
     private String name;
-    private List<T> weapons;
+    private List<Weapon> weapons;
 
     public Person(String name) {
         this.name = name;
+        this.weapons = new ArrayList<>();
     }
 
-    public Person(String name, List<T> weapons) {
+    public Person(String name, List<Weapon> weapons) {
         this.name = name;
         this.weapons = weapons;
     }
@@ -29,7 +31,7 @@ public class Person<T extends Weapon> {
         return name;
     }
 
-    public List<T> getWeapons() {
+    public List<Weapon> getWeapons() {
         return weapons;
     }
 }
